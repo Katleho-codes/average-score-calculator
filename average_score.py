da42161afcec76201222f21acb8aa698c71f152e
Katleho-codes <67188889+Katleho-codes@users.noreply.github.com>
2021-06-15 09:54:24 +0200

    Add files via upload

---
+++ average_score.py
@@ -0,0 +1,7 @@
+# Create a file called “average_score.py”, which requests three values and returns the average.
+a = int(input('First number: '))
+b = int(input('Second number: '))
+c = int(input('Third number: '))
+
+theAverage = a + b + c / 3
+print('The average is ' + str(theAverage))
